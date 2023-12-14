a2f9ee8285dc902244621badbcbb93d77a443445
Mattias Nordqvist <mattias.nordqvist@spinit.se>
2023-12-14 16:20:41 +0100

    .

---
+++ AdventOfCode/Day13.cs
@@ -0,0 +1,157 @@
+﻿
+
+namespace AdventOfCode;
+public class Day13 : MyBaseDay
+{
+    public override async ValueTask<string> Solve_1()
+    {
+        var lines = Input.Zplit();
+        var maps = CreateMaps(lines).ToList();
+        return maps.Sum(x => Evaluate(x)!.Value).ToString();
+    }
+
+    private long? Evaluate(Dictionary<P2D, char> map, long? ignore = null)
+    {
+        for (int x = 1; x <= map.MaxBy(p => p.Key.x).Key.x - 1; x++)
+        {
+            if (AreColumnsEqual(x, x + 1, map))
+            {
+                if (ColumnsSurroundingAreEqual(x, x + 1, map))
+                {
+                    if(x == ignore)
+                    {
+                        continue;
+                    }
+                    return x;
+                }
+            }
+        }
+
+        for (int y = 1; y <= map.MaxBy(p => p.Key.y).Key.y - 1; y++)
+        {
+            if (AreRowsEqual(y, y + 1, map))
+            {
+                if (RowsSurroundingAreEqual(y, y + 1, map))
+                {
+                    if (y*100 == ignore)
+                    {
+                        continue;
+                    }
+                    return y * 100;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private bool ColumnsSurroundingAreEqual(int x, int x2, Dictionary<P2D, char> map)
+    {
+        while (true)
+        {
+            if (AreColumnsEqual(x, x2, map))
+            {
+                x--;
+                x2++;
+                if (!map.Keys.Any(k => k.x == x) || !map.Keys.Any(k => k.x == x2))
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    private bool RowsSurroundingAreEqual(int y, int y2, Dictionary<P2D, char> map)
+    {
+        while (true)
+        {
+            if (AreRowsEqual(y, y2, map))
+            {
+                y--;
+                y2++;
+                if (!map.Keys.Any(k => k.y == y) || !map.Keys.Any(k => k.y == y2))
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    private bool AreRowsEqual(int y, int y2, Dictionary<P2D, char> map)
+    {
+        for (int x = 1; x <= map.MaxBy(p => p.Key.x).Key.x; x++)
+        {
+            if (map[new(x, y)] != map[new(x, y2)])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool AreColumnsEqual(int x, int x2, Dictionary<P2D, char> map)
+    {
+        for (int y = 1; y <= map.MaxBy(p => p.Key.y).Key.y; y++)
+        {
+            if (map[new(x, y)] != map[new(x2, y)])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private IEnumerable<Dictionary<P2D, char>> CreateMaps(string[] lines)
+    {
+        Dictionary<P2D, char> keyValuePairs = new Dictionary<P2D, char>();
+        int y = 0;
+        foreach (var line in lines)
+        {
+            for (int x = 0; x < line.Length; x++)
+            {
+                keyValuePairs[new(x + 1, y + 1)] = line[x];
+            }
+
+            y++;
+            if (line.Length == 0)
+            {
+                yield return keyValuePairs;
+                keyValuePairs = new Dictionary<P2D, char>();
+                y = 0;
+            }
+        }
+        yield return keyValuePairs;
+
+    }
+
+    public override async ValueTask<string> Solve_2()
+    {
+        var lines = Input.Zplit();
+        var maps = CreateMaps(lines).ToList();
+        return maps.Sum(Evaluate2).ToString();
+    }
+
+    private long Evaluate2(Dictionary<P2D, char> map)
+    {
+        var original = Evaluate(map)!.Value;
+
+        foreach (var p in map)
+        {
+            var newDictionary = map.ToDictionary(x => x.Key, x => x.Key == p.Key ? x.Value == '.' ? '#' : '.' : x.Value);
+            var newReflection = Evaluate(newDictionary, ignore: original);
+            if (newReflection != null && newReflection.Value != original)
+            {
+                return newReflection.Value;
+            }
+        }
+        return original;
+    }
+}
---
+++ AdventOfCode/Day14.cs
@@ -0,0 +1,227 @@
+﻿
+
+namespace AdventOfCode;
+public class Day14 : MyBaseDay
+{
+    public override async ValueTask<string> Solve_1()
+    {
+        var map = Input.ToMap();
+        var maxy = map.Keys.MaxBy(x => x.y).y;
+        var maxx = map.Keys.MaxBy(x => x.x).x;
+        TiltNorth();
+        var load = CalculateLoad(map);
+        
+        return load.ToString();
+        void TiltNorth()
+        {
+            for (int y = 1; y <= maxy; y++)
+            {
+                for (int x = 0; x <= maxx; x++)
+                {
+                    if (map[(new(x, y))] == 'O')
+                    {
+                        var i = 0;
+                        while (y - (i + 1) >= 0 && map[new(x, y - (i + 1))] == '.')
+                        {
+                            i++;
+                        }
+                        if (i != 0)
+                        {
+                            map[(new(x, y))] = '.';
+                            map[(new(x, y - (i)))] = 'O';
+
+                        }
+                    }
+                }
+            }
+        }
+
+        void TiltSouth()
+        {
+            for (int y = maxy-1; y >= 0; y--)
+            {
+                for (int x = 0; x <= maxx; x++)
+                {
+                    if (map[(new(x, y))] == 'O')
+                    {
+                        var i = 0;
+                        while (y + (i + 1) >= 0 && map[new(x, y + (i + 1))] == '.')
+                        {
+                            i++;
+                        }
+                        if (i != 0)
+                        {
+                            map[(new(x, y))] = '.';
+                            map[(new(x, y + (i)))] = 'O';
+
+                        }
+                    }
+                }
+            }
+        }
+        int CalculateLoad(Dictionary<P2D, char> map)
+        {
+            return map.Where(x => x.Value == 'O').Sum(x => maxy - x.Key.y + 1);
+        }
+    }
+
+    public override async ValueTask<string> Solve_2()
+    {
+        var map = Input.ToMap();
+        var maxy = map.Keys.MaxBy(x => x.y).y;
+        var maxx = map.Keys.MaxBy(x => x.x).x;
+        var totalCycles = 1000000000;
+        var warmup = 500;
+        List<int> sequence = new List<int>();
+        for (int a = 0;a < warmup; a++)
+        {
+            Cycle();
+            sequence.Add(CalculateLoad(map));
+        }
+        var (cycleStart, cycleLength) = DetectCycle(sequence);
+        totalCycles -= warmup;
+        totalCycles = totalCycles % cycleLength;
+        for(int a = 0; a <totalCycles; a++)
+        {
+            Cycle();
+        }
+
+        var load = CalculateLoad(map);
+        void Cycle()
+        {
+            TiltNorth();
+            TiltWest();
+            TiltSouth();
+            TiltEast();
+        }
+        return load.ToString();
+        void TiltNorth()
+        {
+            for (int y = 1; y <= maxy; y++)
+            {
+                for (int x = 0; x <= maxx; x++)
+                {
+                    if (map[(new(x, y))] == 'O')
+                    {
+                        var i = 0;
+                        while (y - (i + 1) >= 0 && map[new(x, y - (i + 1))] == '.')
+                        {
+                            i++;
+                        }
+                        if (i != 0)
+                        {
+                            map[(new(x, y))] = '.';
+                            map[(new(x, y - (i)))] = 'O';
+
+                        }
+                    }
+                }
+            }
+        }
+
+        void TiltWest()
+        {
+            for (int x = 1; x <= maxx; x++)
+            {
+                for (int y = 0; y <= maxy; y++)
+                {
+                    if (map[(new(x, y))] == 'O')
+                    {
+                        var i = 0;
+                        while (x - (i + 1) >= 0 && map[new(x - (i + 1), y)] == '.')
+                        {
+                            i++;
+                        }
+                        if (i != 0)
+                        {
+                            map[(new(x, y))] = '.';
+                            map[(new(x - (i), y))] = 'O';
+
+                        }
+                    }
+                }
+            }
+        }
+
+        void TiltSouth()
+        {
+            for (int y = maxy - 1; y >= 0; y--)
+            {
+                for (int x = 0; x <= maxx; x++)
+                {
+                    if (map[(new(x, y))] == 'O')
+                    {
+                        var i = 0;
+                        while (y + (i + 1) <= maxy && map[new(x, y + (i + 1))] == '.')
+                        {
+                            i++;
+                        }
+                        if (i != 0)
+                        {
+                            map[(new(x, y))] = '.';
+                            map[(new(x, y + (i)))] = 'O';
+
+                        }
+                    }
+                }
+            }
+        }
+
+        void TiltEast()
+        {
+            for (int x = maxx - 1; x >= 0; x--)
+            {
+                for (int y = 0; y <= maxy; y++)
+                {
+                    if (map[(new(x, y))] == 'O')
+                    {
+                        var i = 0;
+                        while (x + (i + 1) <= maxx && map[new(x + (i + 1), y)] == '.')
+                        {
+                            i++;
+                        }
+                        if (i != 0)
+                        {
+                            map[(new(x, y))] = '.';
+                            map[(new(x+i, y))] = 'O';
+
+                        }
+                    }
+                }
+            }
+        }
+        int CalculateLoad(Dictionary<P2D, char> map)
+        {
+            return map.Where(x => x.Value == 'O').Sum(x => maxy - x.Key.y + 1);
+        }
+    }
+
+    /// <summary>
+    /// Using the tortoise and hare algorithm to detect a cycle in the sequence
+    /// </summary>
+    public (int cycleStart, int cycleLength) DetectCycle(List<int> sequence)
+    {
+        var hare = 0;
+        var tortoise = 0;
+        while (true)
+        {
+            hare += 2;
+            tortoise += 1;
+            if (sequence[hare] == sequence[tortoise])
+            {
+                tortoise = 0; break;
+            }
+        }
+        var cycleLength = 0;
+        while (true)
+        {
+            cycleLength += 2;
+            hare += 1;
+            tortoise += 1;
+            if(sequence[hare] == sequence[tortoise])
+            {
+                return (tortoise, cycleLength);
+            }
+        }
+    }
+}
